08fbee74e82fa4eb92a853892075229c35ecac85
Antonio <tuj46268@temple.edu>
2024-09-05 16:08:44 -0400

    Changes to new branch

---
+++ newBranchFile.java
@@ -0,0 +1,6 @@
+class newBranchFile{
+    public static void main(String[] args){
+        System.out.println("These are the changes to the file in the new branch of my repository");
+        System.out.println("Changes to be reviewed whn staging");
+    }
+}
